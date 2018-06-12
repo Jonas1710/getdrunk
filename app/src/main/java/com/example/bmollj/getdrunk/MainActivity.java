8a757faeaf1e4ffc81f2bf3140e5a068730e09bf
Mollet Jonas <bmollj@bbc-be.local>
2018-06-12 09:57:09 +0200

    First commit

---
+++ app/src/main/java/com/example/bmollj/getdrunk/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.bmollj.getdrunk;
+
+
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleCursorAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.loading_bars_overview_progress);
+        progressBar.setVisibility(View.VISIBLE);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+
+    }
+
+    private void generateAlertDialog() {
+        progressBar.setVisibility(View.GONE);
+        AlertDialog.Builder dialogBuilder;
+        dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // Closes this activity
+                finish();
+            }
+        });
+        dialogBuilder.setMessage("Die Badidetails konnten nicht geladen werden. Versuche es später nochmals.").setTitle("Fehler");
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+    }
+}
