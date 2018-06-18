4d184d13c91056817f18414758981fd0b454d66d
patricflueckiger <patric.flueckiger@besonet.ch>
2018-06-18 15:47:18 +0200

    Merge

--- app/src/main/java/com/example/bmollj/getdrunk/MainActivity.java
+++ app/src/main/java/com/example/bmollj/getdrunk/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.bmollj.getdrunk;
 
 import android.app.ListActivity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.ActionBar;
@@ -13,9 +15,15 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SimpleCursorAdapter;
@@ -32,13 +40,47 @@ public class MainActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.loading_bars_overview_progress);
         progressBar.setVisibility(View.VISIBLE);
 
+        addBarsToList();
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
 
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+
+
+
+
+    private void addBarsToList() {
+        ListView bars = findViewById(R.id.bars_overview_list);
+        //ArrayAdapter<Bar> barAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1);
+        //barAdapter.addAll(BadiDao.getAll());
+        //bars.setAdapter(barAdapter);
+
+        AdapterView.OnItemClickListener mListClickedHandler = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), Detail.class);
+                //Bar selected = (Bar)parent.getItemAtPosition(position);
+                //intent.putExtra("barId", selected.getId());
+                //intent.putExtra("barName", selected.getName());
+                startActivity(intent);
+            }
+        };
+        bars.setOnItemClickListener(mListClickedHandler);
+    }
+
+
 
     private void generateAlertDialog() {
         progressBar.setVisibility(View.GONE);
